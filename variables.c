e35621edd21f6cc17e87dced4cf175dd5c703b61
Rana Khan <agile.coder@outlook.com>
2024-12-26 23:25:19 +0000

    add more vairbales and compile the code

--- variables.c
+++ variables.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
+    // exercise 1
     int number;
     int test;
     int example;
 
     printf("Hello World!\n");
+
+    // exercise 2
+
+    int studenRank;
+    float studentFeeTotal;
+    char studentGradeLetter;
+
+    // assign values to variables
+    studenRank = 2;
+    studentFeeTotal = 1234.56;
+    studentGradeLetter = 'A';
+
+    //lets print these values
+    printf("Student Rank: %dnd\n", studenRank);
+    printf("Student Fee Total: %4.2f\n", studentFeeTotal);
+    printf("Student Grade Letter: %c\n", studentGradeLetter);
 }
