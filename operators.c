3f354745188e11e1e39c8a37df1883c63fa4e699
Rana Khan <agile.coder@outlook.com>
2024-12-28 23:19:56 +0000

    perform basic arithmetic operations

---
+++ operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  
+  // Variables set for you, do not change here
+  int booksSold = 100;
+  double bookCost = 9.99;
+  double totalSalesValue;
+
+  // Make your changes here
+  //update sales
+  booksSold += 200;
+  //update total sales
+  totalSalesValue = booksSold * bookCost;
+  totalSalesValue /= 2; 
+
+
+
+  // Output logic, no need to change this
+  printf("You sold %d books and your take home was $%.2f\n", booksSold, totalSalesValue);
+}
