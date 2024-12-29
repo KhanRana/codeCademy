9c4cae3961325ea9d988444e98c8db55e4d01f1d
Rana Khan <agile.coder@outlook.com>
2024-12-29 14:17:24 +0000

    basic increment and decrement operations

--- operators.c
+++ operators.c
@@ -2,6 +2,8 @@
 
 int main() {
   
+  // exercise 1
+
   // Variables set for you, do not change here
   int booksSold = 100;
   double bookCost = 9.99;
@@ -18,4 +20,14 @@ int main() {
 
   // Output logic, no need to change this
   printf("You sold %d books and your take home was $%.2f\n", booksSold, totalSalesValue);
+
+  // exercise 2
+
+  int n = 13;
+  int m = 10;
+  
+  m--;
+  n++;
+  printf("m = %d\n", m);
+  printf("n = %d\n", n);
 }
