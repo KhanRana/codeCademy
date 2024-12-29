6079638dc7c3a3932c799ef2d5c6b7e4f3d22bd2
Rana Khan <agile.coder@outlook.com>
2024-12-29 14:50:20 +0000

    add logic to the code

--- operators.c
+++ operators.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-  
+int main()
+{
+
   // exercise 1
 
   // Variables set for you, do not change here
@@ -10,13 +11,11 @@ int main() {
   double totalSalesValue;
 
   // Make your changes here
-  //update sales
+  // update sales
   booksSold += 200;
-  //update total sales
+  // update total sales
   totalSalesValue = booksSold * bookCost;
-  totalSalesValue /= 2; 
-
-
+  totalSalesValue /= 2;
 
   // Output logic, no need to change this
   printf("You sold %d books and your take home was $%.2f\n", booksSold, totalSalesValue);
@@ -24,31 +23,42 @@ int main() {
   // exercise 2: increment & decrement
   int n = 13;
   int m = 10;
-  
+
   m--;
   n++;
   printf("m = %d\n", m);
   printf("n = %d\n", n);
 
-  //exercise 3: find losses
-    int timesServerCrashed = 5;
+  // exercise 3: find losses
+  int timesServerCrashed = 5;
   double losses = 500.95;
-
 
   losses *= timesServerCrashed;
   printf("Total Losses: $%.2f\n", losses);
 
-  //adjust the losses
+  // adjust the losses
   losses /= 2;
   printf("Adjusted Losses: $%.2f\n", losses);
 
-
-  //exercise 4: comparison
-    int x = 5;
+  // exercise 4: comparison
+  int x = 5;
   int y = 42;
 
-  if (x !=  y) {
-   printf("Congratulations on setting up a comparison correctly!");
+  if (x != y)
+  {
+    printf("Congratulations on setting up a comparison correctly!");
   }
 
+  // exercise 5: logical operators
+  int x = 1;
+  int y = 27;
+
+  if (x != y)
+  {
+    printf("Congratulations on setting up a comparison correctly!");
+  }
+  else
+  {
+    printf("Please try again!");
+  }
 }
