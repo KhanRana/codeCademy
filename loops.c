de0816248b2f870c245d116682640dd12ab52567
Rana Khan <agile.coder@outlook.com>
2024-12-29 22:49:47 +0000

    add code to break out of a loop

--- loops.c
+++ loops.c
@@ -32,4 +32,35 @@ int main()
         printf("%d\n", i);
         i++;
     } while (i < 10);
+
+    // break out of a loop
+
+    int number1 = 1;
+    int numbers_entered1 = 0;
+
+    int number2 = 1;
+    int numbers_entered2 = 0;
+
+    while (numbers_entered1 < 10)
+    {
+
+        printf("Loop 1 - Please enter a number: ");
+        scanf("%d", &number1);
+
+        // Figure out how to break out here!
+        if (number1 <= 0)
+        {
+            break;
+        }
+        numbers_entered1++;
+    }
+    printf("Good job! You’ve broken out!\n");
+
+    while (number2 > 0 && numbers_entered2 < 10)
+    {
+        printf("Loop 1 - Please enter a number: ");
+        scanf("%d", &number2);
+        numbers_entered2++;
+    }
+    printf("Good job! You’ve broken out!\n");
 }
