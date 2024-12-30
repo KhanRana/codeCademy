bf66dd745ec91560c1de44077db72822a8b84c25
Rana Khan <agile.coder@outlook.com>
2024-12-30 17:23:06 +0000

    create string by using array of chars and string literals

--- strings.c
+++ strings.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main (void) {
-    
+int main(void)
+{
+    // Checkpoint 1 code goes here
+    char comp[] = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r', '\0'};
+
+    // Checkpoint 2 code goes here
+
+    char compCode[] = "Computer Code";
+
+    // Checkpoint 3 code goes here
+
+    printf("%s\n", comp);
+    printf("%s\n", compCode);
 }
