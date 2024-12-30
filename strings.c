fd0777fac0e0899e29b674fe8414f92b124e74b2
Rana Khan <agile.coder@outlook.com>
2024-12-30 17:28:30 +0000

    access and modify characters in a string

--- strings.c
+++ strings.c
@@ -13,4 +13,13 @@ int main(void)
 
     printf("%s\n", comp);
     printf("%s\n", compCode);
+
+    // character access and modification
+    char p[] = "procezzor";
+    // Checkpoint 1 code goes here
+    printf("%c\n", p[2]);
+    // Checkpoint 2 code goes here
+    p[5] = 's';
+    p[6] = 's';
+    printf("%s", p);
 }
