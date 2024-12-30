54d7636a21d9f86ee3fb1e5f1c8e5a7e02e632ef
Rana Khan <agile.coder@outlook.com>
2024-12-30 14:40:48 +0000

    format the doc

--- arrays.c
+++ arrays.c
@@ -21,7 +21,7 @@ int main(void)
     // mutidimensional arrays
 
     // Checkpoint 1 code goes here.
-    int arr[9][9];
+    // int arr[9][9];
 
     // Checkpoint 2 code goes here.
     // int matrix[3][3] = {{2, 9, 11}, {3, 1, 4}, {8, 3, 1}};
