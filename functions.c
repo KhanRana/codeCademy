8594529f0f6fcc6a8f0931513d54ceadf7fc16b1
Rana Khan <agile.coder@outlook.com>
2024-12-31 13:28:18 +0000

    create function to generate random number between 1 and 1000 inclusive

--- functions.c
+++ functions.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // define a funtion that prints a string
 void printMyFavoriteAnimal(void)
 {
     printf("My favorite animal is a Tiger\n");
 }
+
+//random number generator
+int getRandom1000() {
+  int random1000 = rand() % 1000 + 1;
+  return random1000;
+}
+
 
 int main(void)
 {
@@ -14,4 +22,9 @@ int main(void)
 
     //call the function
     printMyFavoriteAnimal();
+
+    //generate random number
+    srand(time(NULL));
+    int random1000 = getRandom1000();
+    printf("%d\n", random1000);
 }
