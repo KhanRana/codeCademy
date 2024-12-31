f737ebb9fc6d016e6104e24d82052062e84aef93
Rana Khan <agile.coder@outlook.com>
2024-12-31 13:16:24 +0000

    define a function to print your favourite animal and call it

--- functions.c
+++ functions.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+// define a funtion that prints a string
+void printMyFavoriteAnimal(void)
+{
+    printf("My favorite animal is a Tiger\n");
+}
+
+int main(void)
+{
     int absValue = abs(-5);
     printf("%d\n", absValue);
+
+    //call the function
+    printMyFavoriteAnimal();
 }
